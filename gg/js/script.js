e4af2bf678679ed621aac41a6d7a2a7e83d24f1c
firay <maschenko2012@gmail.com>
2018-10-30 16:25:17 +0200

    1

--- gg/js/script.js
+++ gg/js/script.js
@@ -30,7 +30,7 @@ $(function(){
 // Menu js for Position fixed
     $(window).scroll(function(){
       var window_top = $(window).scrollTop() + 2; 
-        if (window_top > 725){
+        if (window_top > 700){
           $('.nav-ul').addClass('nav-ul_fixed animated fadeInDown');
         } else {
           $('.nav-ul').removeClass('nav-ul_fixed animated fadeInDown');
